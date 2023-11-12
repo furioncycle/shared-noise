2e624f60f997daaa25952791af6e8da9675c415f
ttecho <ttecho2021@gmail.com>
2023-11-11 19:15:52 -0800

    shadow for the header and starting to get a parallax started

--- src/app/routes/home.rs
+++ src/app/routes/home.rs
@@ -2,5 +2,9 @@ use leptos::*;
 
 #[component]
 pub fn Home() -> impl IntoView {
-    view! { <div class="parallax" style="padding-bottom: 100%;"></div> }
+    view! {
+        <div class="parallax" style="padding-bottom: 100%;">
+            <div class="home" style="padding-top: 60px;">"TODO"</div>
+        </div>
+    }
 }
